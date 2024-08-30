c329ce504a8a245cd670c4aec9d02923c766bb4d
pntmernbatchthree <pntmernbatchthree@gmail.com>
2024-08-30 17:28:36 +0600

    error resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import ProductList from "@/components/ProductList";
 import { getData } from "@/lib";
 
 export default async function Home() {
-  const endpoint = "https://dummyjson.com/products";
-  const products = await getData(endpoint);
+  // const endpoint = "https://dummyjson.com/products";
+  const products = await getData("https://dummyjson.com/products");
 
   return (
     <main>
